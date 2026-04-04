64e6598d9f6ca624347623f537a73dcefb9f7dc0
LeishaH <210328815+LeishaH@users.noreply.github.com>
2026-04-04 13:50:17 +1100

    Refactor weather component layout using flexbox and improve spacing

--- src/Weather.js
+++ src/Weather.js
@@ -4,16 +4,16 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
-      <form className="mb-3">
+      <form className="mb-4">
         <div className="row">
           <div className="col-9">
             <input
               type="search"
               placeholder="Enter a city.."
               className="form-control"
+              autoFocus
             />
           </div>
-
           <div className="col-3">
             <input
               type="submit"
@@ -24,29 +24,33 @@ export default function Weather() {
         </div>
       </form>
 
-      <h1>New York</h1>
-
-      <ul>
-        <li>Wednesday 07:00</li>
-        <li>Mostly Cloudy</li>
-      </ul>
-
-      <div className="row">
-        <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="Mostly Cloudy"
-          />
-          <span className="temperature">6</span>
-          <span className="unit">°C</span>
+      <div className="weather-body">
+        <div className="overview">
+          <h1>New York</h1>
+          <ul>
+            <li>Wednesday 07:00</li>
+            <li>Mostly Cloudy</li>
+          </ul>
         </div>
 
-        <div className="col-6">
-          <ul>
-            <li>Precipitation: 15%</li>
-            <li>Humidity: 72%</li>
-            <li>Wind: 13 km/h</li>
-          </ul>
+        <div className="weather-data">
+          <div className="weather-temperature">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="Mostly Cloudy"
+              className="weather-icon"
+            />
+            <span className="temperature">6</span>
+            <span className="unit">°C</span>
+          </div>
+
+          <div className="weather-details">
+            <ul>
+              <li>Precipitation: 15%</li>
+              <li>Humidity: 72%</li>
+              <li>Wind: 13 km/h</li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
